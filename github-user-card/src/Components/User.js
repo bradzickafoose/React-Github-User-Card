2196176b5fcf0aa18658b9f481c42fb0689d4c66
Brad <brad@viziworx.com>
2020-03-18 11:56:01 -0400

    removes unnecessary parenthesis from calback functions and makes console logs more descriptive

--- github-user-card/src/Components/User.js
+++ github-user-card/src/Components/User.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function User(props) {
-	console.log('User:', props);
-	return (
-		<div className='User-card' key='props.key'>
-			<a href={props.url}>
-				<img src={props.image} alt={props.header} />
-				<h1>{props.header}</h1>
-				<p>{props.subheader}</p>
-			</a>
-		</div>
-	);
+    console.log("User component props:", props);
+    return (
+        <div className="User-card" key="props.key">
+            <a href={props.url}>
+                <img src={props.image} alt={props.header} />
+                <h1>{props.header}</h1>
+                <p>{props.subheader}</p>
+            </a>
+        </div>
+    );
 }
 
 export default User;
--- github-user-card/src/Components/UserList.js
+++ github-user-card/src/Components/UserList.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import User from './User';
+import React from "react";
+import User from "./User";
 
 function UserList(props) {
-	console.log('UserList:', props);
-	return (
-		<div className='User-list'>
-			<User
-				image={props.user.avatar_url}
-				header={props.user.name}
-				subheader={props.user.login}
-				url={props.user.html_url}
-			/>
-			{props.followers.map((follower) => (
-				<User key={follower.id} image={follower.avatar_url} header={follower.login} url={follower.html_url} />
-			))}
-		</div>
-	);
+    console.log("UserList component props:", props);
+    return (
+        <div className="User-list">
+            <User
+                image={props.user.avatar_url}
+                header={props.user.name}
+                subheader={props.user.login}
+                url={props.user.html_url}
+            />
+            {props.followers.map(follower => (
+                <User
+                    key={follower.id}
+                    image={follower.avatar_url}
+                    header={follower.login}
+                    url={follower.html_url}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default UserList;
